7f55762716e771edf536bf6b96bcc0e236baab87
shirokoweb <jacklinkers@gmail.com>
2023-03-19 15:16:55 +0100

    Update webplus-footnotes.php

--- webplus-footnotes.php
+++ webplus-footnotes.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: WebPlus FootNotes
- * Plugin URI: https://webplus.agency
+ * Plugin URI: https://github.com/shirokoweb/webplus-footnotes/
  * Description: This plugin adds footnotes to articles.
- * Version: 4.0.0
- * Author: Jacques Mojsilovic
+ * Version: 1.0.0
+ * Author: shirokoweb
  * Author URI: https://webplus.agency
  * License: GPL-3.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
